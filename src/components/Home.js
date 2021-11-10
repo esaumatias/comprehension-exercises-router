7e22b2085fa608fb4b7ad68a268c3b0be81f5f03
Esau Matias <esaufreirematias@gmail.com>
2021-11-10 16:05:59 -0300

    finaliza requisito 3, 4, 5 e 6

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
-
+import { Link } from 'react-router-dom';
 class Home extends Component {
   render() {
     return (
       <div>
+        <Link to="/About">About</Link>
+        <Link to="/Users">Users</Link>
         <h2>Home</h2>
         <p> My awesome Home component </p>
       </div>
