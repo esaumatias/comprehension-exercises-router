a63e273ab75e5bdf702db847c922436ce4287a6a
Esau Matias <esaufreirematias@gmail.com>
2021-11-10 15:53:47 -0300

    finaliza requisito 2 e 3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import { BrowserRouter, Route } from 'react-router-dom';
 import React, { Component } from 'react';
 import Home from './components/Home';
-import { BrowserRouter } from 'react-router-dom';
+import About from './components/About'
+import Users from './components/Users'
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Home />
+        <Route exact path="/" component={Home} />
+        <Route path="/About" component={About} />
+        <Route path="/Users" component={Users} />
       </BrowserRouter>
     );
   }
